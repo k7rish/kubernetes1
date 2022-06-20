2163fe197ae8c89012cf98e68108df7e69a48dff
Ubuntu <ubuntu@ip-10-1-0-231.ec2.internal>
2022-06-20 07:19:38 +0000

    First

---
+++ script.sh
@@ -0,0 +1,8 @@
+kubectl delete deploy newdep nginx-deployment
+kubectl delete service my-service newdep
+kubectl delete ing example-ingress
+kubectl create -f deploy.yaml
+kubectl create -f ser.yaml
+kubectl create -f deploy2.yaml
+kubectl create -f ser2.yaml
+kubectl create -f gni.yaml
