3c7222373f5eba96f78a715113e18aa8d19b1b1e
k7rish <100378183+k7rish@users.noreply.github.com>
2022-06-22 18:49:36 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,3 @@
-kubectl delete deploy newdep nginx-deployment
-kubectl delete service my-service newdep
-kubectl delete ing example-ingress
-kubectl create -f deploy.yaml
-kubectl create -f ser.yaml
-kubectl create -f deploy2.yaml
-kubectl create -f ser2.yaml
-kubectl create -f gni.yaml
-kubectl create -f ng1.yaml
+ssh root@44.200.130.239 "
+kubect get svc
+"
